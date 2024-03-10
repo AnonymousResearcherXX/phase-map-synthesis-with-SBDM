3c579d0e66eabac6ab6d89d305a2ac49bb5fb7c5
AnonymousResearcherXX <162626323+AnonymousResearcherXX@users.noreply.github.com>
2024-03-09 22:27:05 -0500

    Add files via upload

---
+++ configs/cardiac_default_configs.py
@@ -0,0 +1,67 @@
+import ml_collections
+import torch
+
+
+def get_default_configs():
+  config = ml_collections.ConfigDict()
+  # training
+  config.training = training = ml_collections.ConfigDict()
+  config.training.batch_size = 128
+  # training.n_iters = 2400001
+  training.epochs = 100 # 100
+  training.snapshot_freq = 50000
+  training.log_freq = 50
+  #training.log_freq = 25
+  training.eval_freq = 100
+  ## store additional checkpoints for preemption in cloud computing environments
+  training.snapshot_freq_for_preemption = 5000
+  ## produce samples at each snapshot.
+  training.snapshot_sampling = True
+  training.likelihood_weighting = False
+  training.continuous = True
+  training.reduce_mean = False
+
+  # sampling
+  config.sampling = sampling = ml_collections.ConfigDict()
+  sampling.n_steps_each = 1
+  sampling.noise_removal = True
+  sampling.probability_flow = False
+  sampling.snr = 0.075
+
+  # data
+  config.data = data = ml_collections.ConfigDict()
+  data.type = "cardiac"
+  data.image_size = 128
+  data.uniform_dequantization = False
+  data.centered = False
+  data.num_channels = 2
+
+  # model
+  config.model = model = ml_collections.ConfigDict()
+  model.sigma_max = 378
+  model.sigma_min = 0.01
+  model.num_scales = 2000
+  model.beta_min = 0.1
+  model.beta_max = 20.
+  model.dropout = 0.
+  model.embedding_type = 'fourier'
+
+  # optimization
+  config.optim = optim = ml_collections.ConfigDict()
+  optim.weight_decay = 0
+  optim.optimizer = 'Adam'
+  optim.lr = 2e-4
+  optim.beta1 = 0.9
+  optim.eps = 1e-8
+  optim.warmup = 5000
+  optim.grad_clip = 1.
+
+  # position encoding 
+  config.pos_emb = False # positional encoding (like time conditioning)
+  config.pos_emb2 = False # positional encoding vol. 2 (concatenating position encoding to input)
+  #config.demb = 128
+
+  config.seed = 101
+  config.device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+  return config
